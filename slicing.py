1caf88f7932c0d62a2a86c1f3b7e4b17b7ce1ba4
shubh060298 <51718163+shubh060298@users.noreply.github.com>
2021-09-29 04:00:40 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,14 @@
+str = "string slicing"
+print(str[5:11])
+print(str[3:10])
+print(str[7::2])
+
+a="welcome to my blog"
+print(a[3:])
+print(a[2:13:2])
+print(a[:6])
+print(a[8:-1])
+print(a[:7:3])
+print(a[4:6]+" "+a[-3::2])
+print(a[-6])
+print(a[8::3])
